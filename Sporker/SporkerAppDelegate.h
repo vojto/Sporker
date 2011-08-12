1ca871a0df9ea7eb04831573ca6dec73aa900686
Vojto Rinik <vojto@rinik.net>
2011-08-12 15:34:42 -0400

    Initial Commit

---
+++ Sporker/SporkerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SporkerAppDelegate.h
+//  Sporker
+//
+//  Created by Vojto Rinik on 8/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SporkerAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Sporker/SporkerAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  SporkerAppDelegate.m
+//  Sporker
+//
+//  Created by Vojto Rinik on 8/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SporkerAppDelegate.h"
+
+@implementation SporkerAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
